ab376888c2256f106a354236dbd47fca677c567a
Abraham Castellon <aycast@ucdavis.edu>
2025-12-21 01:25:23 -0800

    incomplete stats implementation

--- Scripts/nathangarcia/nathangarcia.gd
+++ Scripts/nathangarcia/nathangarcia.gd
@@ -1,42 +1,44 @@
 extends CharacterBody2D
 class_name nathangarcia
 
-@export var health : int
+@export var health : int = 100
 @export var SPEED := 300.0
 @export var JUMP_VELOCITY := -400.0
 
 @onready var healthbar = $Healthbar
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var direction : float = 0.0
+@onready var is_alive : bool = true
 
 func _ready():
-	health = 5
-	#on_dead = die 
 	healthbar.init_health(health)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	if is_alive:
+		# Add the gravity.
+		if not is_on_floor():
+			velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+		# Handle jump.
+		if Input.is_action_just_pressed("up") and is_on_floor():
+			velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	direction = Input.get_axis("left", "right")
-	if direction:
-		if Input.is_action_pressed("down"): # Speed when crouch
-			velocity.x = direction * (SPEED / 2)
+		# Get the input direction and handle the movement/deceleration.
+		# As good practice, you should replace UI actions with custom gameplay actions.
+		direction = Input.get_axis("left", "right")
+		if direction:
+			if Input.is_action_pressed("down"): # Speed when crouch
+				velocity.x = direction * (SPEED / 2)
+			else:
+				velocity.x = direction * SPEED
 		else:
-			velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+			velocity.x = move_toward(velocity.x, 0, SPEED)
 
-	move_and_slide()
+		move_and_slide()
 
-#func _set_health(value):
-	#super._set_health(value)
-	#if health <= 0 && is_alive:
-		#_die()
+
+func _update_health(value) -> void:
+	health = value
+	healthbar.health = health
+	if health <= 0 && is_alive:
+		is_alive = false
--- Scripts/nathangarcia/stats.gd
+++ Scripts/nathangarcia/stats.gd
@@ -1,24 +1,55 @@
 extends Resource
 class_name Stats
 
+enum BuffableStats {
+	MAX_HEALTH,
+	DEFENSE,
+	ATTACK,
+}
+
+const STAT_CURVES: Dictionary[BuffableStats, Curve] = {
+	BuffableStats.MAX_HEALTH: preload("uid://cykt0qhs0rp6l"),
+	BuffableStats.DEFENSE: preload("uid://ep3fxl1ai5mw"),
+	BuffableStats.ATTACK: preload("uid://c0dakcdtacte"),
+}
+
+const BASE_LEVEL_XP: float = 100.0
+
 signal health_depleted
 signal health_changed(cur_health: int, max_health: int)
 
 @export var base_max_health: int = 100
 @export var base_defense: int = 10
 @export var base_attack: int = 10
+@export var experience: int = 0: set = _on_experience_set
 
+var level: int:
+	get(): return floor(max(1.0, sqrt(experience/ BASE_LEVEL_XP) + 0.5))
 var curr_max_health: int = 100
 var curr_defense: int = 10
 var curr_attack: int = 10
 
 var health: int =0: set = _on_health_set
 
+var stat_buffs: Array[StatBuff]
+
 func _init() -> void:
 	setup_stats.call_deferred()
 	
 func setup_stats() -> void:
+	# recalculate the current stats first
+	recalculate_stats()
 	health = curr_max_health
+	
+func add_buff(buff: StatBuff) -> 
+	
+func recalculate_stats() -> void:
+	var stat_sample_pos: float = (float(level) / 100.0) - 0.01
+	curr_max_health = base_max_health * STAT_CURVES[BuffableStats.MAX_HEALTH].sample(stat_sample_pos)
+	curr_defense = base_defense * STAT_CURVES[BuffableStats.DEFENSE].sample(stat_sample_pos)
+	curr_attack = base_attack * STAT_CURVES[BuffableStats.ATTACK].sample(stat_sample_pos)
+	
+	
 	
 	
 func _on_health_set(new_value: int) -> void:
@@ -26,3 +57,10 @@ func _on_health_set(new_value: int) -> void:
 	health_changed.emit(health, curr_max_health)
 	if health <= 0:
 		health_depleted.emit()
+
+func _on_experience_set(new_value: int) -> void:
+	var old_level: int = level
+	experience = new_value 
+	
+	if not old_level == level:
+		recalculate_stats()
